2d29e803a569f121b8c6bcfe8df19f340b4cb7eb
ulfjoh <ulf.k.johansson@gmail.com>
2025-07-26 17:30:22 -0400

    Creating VPC via modules.

--- main.tf
+++ main.tf
@@ -1,3 +1,52 @@
+# VPC + EC2 with Modules
+
+provider "aws" {
+  region = "us-west-2"  # or your preferred region
+}
+
+# VPC using official module
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.1.2"
+
+  name = "blog-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a"]
+  public_subnets  = ["10.0.1.0/24"]
+
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  enable_nat_gateway = false
+  single_nat_gateway = false
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+# Security group
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.3.0"
+
+  name   = "blog_module"
+  vpc_id = module.vpc.vpc_id
+
+  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules        = ["all-all"]
+  egress_cidr_blocks  = ["0.0.0.0/0"]
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+# AMI lookup
 data "aws_ami" "app_ami" {
   most_recent = true
 
@@ -14,29 +63,15 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-data "aws_vpc" "default" {
-  default = true
-}
-
+# EC2 instance
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
+  subnet_id              = 
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
     Name = "HelloWorld"
   }
-}
-
-module "blog_sg" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "5.3.0"
-  name = "blog_module"
-
-  vpc_id              = data.aws_vpc.default.id
-  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-  egress_rules        = ["all-all"]
-  egress_cidr_blocks  = ["0.0.0.0/0"]
 }
