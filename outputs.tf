175980aa57569cf5c26132d812bd7acea0a07fa9
ulfjoh <ulf.k.johansson@gmail.com>
2025-07-26 16:10:19 -0400

    Added more output fields

--- outputs.tf
+++ outputs.tf
@@ -6,3 +6,32 @@ output "instance_arn" {
   value = aws_instance.blog.arn
 }
 
+output "instance_id" {
+  description = "The ID of the EC2 instance"
+  value       = aws_instance.blog.id
+}
+
+output "instance_arn" {
+  description = "The ARN of the EC2 instance"
+  value       = aws_instance.blog.arn
+}
+
+output "instance_public_ip" {
+  description = "The public IP address of the instance"
+  value       = aws_instance.blog.public_ip
+}
+
+output "instance_private_ip" {
+  description = "The private IP address of the instance"
+  value       = aws_instance.blog.private_ip
+}
+
+output "instance_az" {
+  description = "The availability zone of the instance"
+  value       = aws_instance.blog.availability_zone
+}
+
+output "security_group_id" {
+  description = "The ID of the security group"
+  value       = aws_security_group.blog.id
+}
