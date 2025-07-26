c6ad98fe0e5292a2fb0adeb03e0087d022d46f9b
ulfjoh <ulf.k.johansson@gmail.com>
2025-07-26 16:13:04 -0400

    Added public DNS information

--- outputs.tf
+++ outputs.tf
@@ -31,3 +31,8 @@ output "security_group_id" {
   description = "The ID of the security group"
   value       = aws_security_group.blog.id
 }
+
+output "instance_public_dns" {
+  description = "The public DNS name of the EC2 instance"
+  value       = aws_instance.blog.public_dns
+}
