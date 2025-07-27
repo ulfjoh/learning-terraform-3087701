e12c66064be8788bac0ab6ea5971cdceca391959
ulfjoh <ulf.k.johansson@gmail.com>
2025-07-27 18:28:07 -0400

    ChatGPT test

--- main.tf
+++ main.tf
@@ -51,10 +51,9 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-# Auto Scaling with Launch Template
 module "blog_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
-  version = "7.6.0"
+  version = "9.0.1"
 
   name                = "blog_asg"
   min_size            = 1
@@ -62,11 +61,12 @@ module "blog_autoscaling" {
   desired_capacity    = 1
   vpc_zone_identifier = module.blog_vpc.public_subnets
 
-  create_launch_template                 = true
-  launch_template_name                   = "blog-launch-template"
-  launch_template_image_id              = data.aws_ami.app_ami.id
-  launch_template_instance_type         = var.instance_type
-  launch_template_vpc_security_group_ids = [module.blog_sg.security_group_id]
+  launch_template = {
+    name_prefix            = "blog-"
+    image_id               = data.aws_ami.app_ami.id
+    instance_type          = var.instance_type
+    vpc_security_group_ids = [module.blog_sg.security_group_id]
+  }
 
   tags = {
     Name = "HelloWorld"
--- providers.tf
+++ providers.tf
@@ -1,11 +1,28 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-    }
-  }
-}
+#terraform {
+# required_providers {
+#    aws = {
+#      source  = "hashicorp/aws"
+#    }
+#  }
+#}
 
 provider "aws" {
   region  = "us-west-2"
+}
+
+terraform {
+  required_version = ">= 1.12.2"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0" # Or your preferred compatible version
+    }
+  }
+
+  experiments = [module_variable_optional_attrs]
+}
+
+provider "aws" {
+  region = "us-west-2"
 }
